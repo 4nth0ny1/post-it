9735c9caf1dbb137cc4e1f1f4ed4b9b7cb390bb8
4nth0ny1 <acatullo4@gmail.com>
2021-08-21 16:01:19 -0500

    add styles to importance, but doesnt work

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,25 +1,30 @@
-
+function ColorImportance(props){
+    document.querySelector('.post-it-note-content').style.color = 'red'
+}
 
 const Post = (props) => {
+    const imp = props.note.importance
+  
+    if (imp === "high"){
+        return (
+            <>
+                <div className="post-it-note">
+                    <p className="post-it-note-content">{props.note.content}</p>
+                </div>
+                <ColorImportance importance={imp}/>;
+            </>
+        )
 
-    const colorImportance = (props) => {
-        if (props.note.importance === 'high') {
-            props.note.importance.style.color = 'red'
-        } else if (props.note.importance === 'medium') {
-            props.note.importance.style.color = 'yellow'
-        } else if (props.note.importance === 'low') {
-            props.note.importance.style.color = 'green'
-        }
-    }
-
-    return (
-        <>
-            <div className="post-it-note">
-                <p className="post-it-note-content">{props.note.content}</p>
-                <p className="post-it-note-content">{colorImportance}</p>
-            </div>
-        </>
-    );
+    } else {
+        return (
+            <>
+                <div className="post-it-note">
+                    <p className="post-it-note-content">{props.note.content}</p>
+                    <p className="post-it-note-content importance">{props.note.importance}</p>
+                </div>
+            </>
+        )
+    }  
 }
 
 export default Post;

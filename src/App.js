137462f35b40c1f4b57dc052bf34640883b3af75
4nth0ny1 <acatullo4@gmail.com>
2021-08-21 17:35:50 -0500

    add finished styles to importance

--- src/App.js
+++ src/App.js
@@ -2,40 +2,60 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import PostContainer from './components/PostContainer';
+import { useState } from 'react';
 
 function App() {
 
-  const notes = [
+  const [ notes, setNotes ] = useState([
     {
       id: 1,
       content: 'take out the trash',
-      importance: 'high'
+      importance: 3
     },
     {
       id: 2,
       content: 'learn about react',
-      importance: 'medium'
+      importance: 2
     },
     {
       id: 3,
       content: 'vacuum', 
-      importance: 'low'
+      importance: 1
     },
     {
       id: 4,
       content: 'talk to mom',
-      importance: 'high'
+      importance: 3
     },
-  ]
+    {
+      id: 5,
+      content: 'sing a song', 
+      importance: 3
+    },
+    {
+      id: 6,
+      content: 'wash dishes', 
+      importance: 2
+    },
+    {
+      id: 7,
+      content: 'read', 
+      importance: 1
+    },
+  ])
 
-  const sortClick = (e) => {
-    
+
+  const handleClick = () => {
+      const sortedNotes = notes.sort( function (a,b) {
+        return b.importance - a.importance
+      })
+      setNotes(sortedNotes)
   }
 
   return (
     <>
       <h1>Post It</h1>
-      <button onClick={sortClick}>Sort</button>
+      <button onClick={handleClick}>Sort</button>
       <PostContainer notes={notes} />
     </>
   );
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,27 +1,36 @@
 const Post = (props) => {
     const imp = props.note.importance
   
-    if (imp === "high"){
+    if (imp === 3){
         return (
             <>
                 <div className="post-it-note">
                     <p className="post-it-note-content">{props.note.content}</p>
-                    <p className="post-it-note-content red">{props.note.importance}</p>
+                    <div className="box high">High</div>
                 </div>
                 
             </>
         )
     
+    } else if (imp === 2) {
+        return (
+            <>
+                <div className="post-it-note">
+                    <p className="post-it-note-content">{props.note.content}</p>
+                    <div className="box med">Med</div>                
+                </div>
+            </>
+        )
     } else {
         return (
             <>
                 <div className="post-it-note">
                     <p className="post-it-note-content">{props.note.content}</p>
-                    <p className="post-it-note-content importance">{props.note.importance}</p>
+                    <div className="box low">Low</div>                
                 </div>
             </>
         )
-    }  
+    }
 }
 
 export default Post;
